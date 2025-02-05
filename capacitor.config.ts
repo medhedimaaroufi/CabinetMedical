afcb41dbdbe2bb25573bc26f8c7a91ce03128b04
medhedimaaroufi <medhedimaaroufi@gmail.com>
2025-02-05 08:57:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CabinetMedical',
+  webDir: 'www'
+};
+
+export default config;
