95e23bf8406ce328e0ac49f0ef5af38b7f235dfa
Chouchenanis <chouchenanis998@gmail.com>
2025-02-12 09:34:11 +0100

    aaaaaa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NotificationComponent } from './notification/notification.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,11 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    redirectTo: 'NotificationComponent',
     pathMatch: 'full'
   },
 ];
