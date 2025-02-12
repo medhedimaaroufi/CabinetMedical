dfbd8f3b589aa05fb6c5465ff035ce2f65b5cd9a
medhedimaaroufi <medhedimaaroufi@gmail.com>
2025-02-12 09:25:37 +0100

    Working on Sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {SigninComponent} from "./signin/signin.component";
 
 const routes: Routes = [
   {
@@ -9,6 +10,11 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: SigninComponent,
     pathMatch: 'full'
   },
 ];
