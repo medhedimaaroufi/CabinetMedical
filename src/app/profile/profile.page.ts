2a14ba69cc3dfd53aa359071b4922566e00f0fd9
unknown <sayadiademali335@gmail.com>
2025-02-12 15:38:53 +0100

    profile1

---
+++ src/app/profile/profile.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
+  imports: [
+    IonicModule
+  ]
+})
+export class ProfilePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
